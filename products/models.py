57e6025fd55ac0d4a4b49b5da54aa53d4a9ebbdc
shayanshokri <shokrishayan1@gmail.com>
2024-08-27 23:11:49 +0330

    create model of Category

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', verbose_name=_('Parent'), blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField(verbose_name=_('Title'), max_length=50)
+    description = models.TextField(verbose_name=_('Description'), blank=True)
+    avatar = models.ImageField(verbose_name=_('Avatar'), blank=True, upload_to='categories')
+    active = models.BooleanField(verbose_name=_('Active'), default=True)
+    created_time = models.DateTimeField(verbose_name=_('Created Time'), auto_now_add=True)
+    updated_time = models.DateTimeField(verbose_name=_('Updated Time'), auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
