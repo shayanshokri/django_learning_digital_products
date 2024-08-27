2a4e65c38cf93a605296adec11a5455ccc8cbd57
shayanshokri <shokrishayan1@gmail.com>
2024-08-27 23:19:30 +0330

    create file model

--- products/models.py
+++ products/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     category = models.ManyToManyField('Category', verbose_name=_('Category'), blank=True)
     title = models.CharField(verbose_name=_('Title'), max_length=50)
     description = models.TextField(verbose_name=_('Description'), blank=True)
-    avatar = models.ImageField(verbose_name=_('Avatar'), blank=True, upload_to='categories')
+    avatar = models.ImageField(verbose_name=_('Avatar'), blank=True, upload_to='categories/')
     active = models.BooleanField(verbose_name=_('Active'), default=True)
     created_time = models.DateTimeField(verbose_name=_('Created Time'), auto_now_add=True)
     updated_time = models.DateTimeField(verbose_name=_('Updated Time'), auto_now=True)
@@ -32,4 +32,15 @@ class Product(models.Model):
         verbose_name_plural = _('Products')
 
 
+class File(models.Model):
+    product = models.ForeignKey('Product', verbose_name=_('Product'), on_delete=models.CASCADE)
+    title = models.CharField(verbose_name=_('Title'), max_length=50)
+    file = models.FileField(verbose_name=_('File'), upload_to='files/%Y/%m/%d')
+    active = models.BooleanField(verbose_name=_('Active'), default=True)
+    created_time = models.DateTimeField(verbose_name=_('Created Time'), auto_now_add=True)
+    updated_time = models.DateTimeField(verbose_name=_('Updated Time'), auto_now=True)
 
+    class Meta:
+        db_table = 'files'
+        verbose_name = _('File')
+        verbose_name_plural = _('Files')
